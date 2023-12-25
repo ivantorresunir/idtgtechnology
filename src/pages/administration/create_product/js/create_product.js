c9991e3e6a1031fc2c8ad2e98afa84b5a115103c
ivantorres96 <ivantorres12@hotmail.com>
2023-12-25 12:51:25 -0500

    Ajuste del consumo.

--- src/pages/administration/create_product/js/create_product.js
+++ src/pages/administration/create_product/js/create_product.js
@@ -3,16 +3,16 @@ import '../css/create_product.css';
 import {DataTable} from "../../../../components/data_table/js/data_table";
 import {useFetch} from "../../../../hooks/fakestoreapi/fakestoreapi_customhook";
 
-let productList = [
-    [21, 'Mouse', '$35.000', 'edit', 'create', 'view'],
-    [22, 'Teclado', '$55.000', 'edit', 'create', 'view'],
-    [23, 'Monitor', '$55.000', 'edit', 'create', 'view'],
-    [24, 'UPS', '$75.000', 'edit', 'create', 'view'],
-    [25, 'Portatil', '$505.000', 'edit', 'create', 'view'],
-    [26, 'Pad Mouse', '$10.000', 'edit', 'create', 'view']
-];
+// let productList = [
+//     [21, 'Mouse', '$35.000', 'edit', 'create', 'view'],
+//     [22, 'Teclado', '$55.000', 'edit', 'create', 'view'],
+//     [23, 'Monitor', '$55.000', 'edit', 'create', 'view'],
+//     [24, 'UPS', '$75.000', 'edit', 'create', 'view'],
+//     [25, 'Portatil', '$505.000', 'edit', 'create', 'view'],
+//     [26, 'Pad Mouse', '$10.000', 'edit', 'create', 'view']
+// ];
 
-// let productList = [];
+let productList = [];
 
 
 const titleListModel = ['#', 'Referencia', 'Precio'];
@@ -25,7 +25,7 @@ export const CreateProduct = () => {
         if (products[0] !== null) {
             products.map((element) => (
                 element.map((elementProduct) => (
-                    productList.push([elementProduct.id, elementProduct.title, '$'+elementProduct.price])
+                    productList.push([elementProduct.id, elementProduct.title, '$' + elementProduct.price])
                 ))
             ));
 
@@ -33,12 +33,14 @@ export const CreateProduct = () => {
         }
     });
 
-    return (
-        <>
-            <div className='container-fluid'>
-                <h1 className='text-center'>Creación de productos</h1>
-                <DataTable title_list={titleListModel} body_list={productList}></DataTable>
-            </div>
-        </>
-    )
+    if (products[0] !== null) {
+        return (
+            <>
+                <div className='container-fluid'>
+                    <h1 className='text-center'>Creación de productos</h1>
+                    <DataTable title_list={titleListModel} body_list={productList}></DataTable>
+                </div>
+            </>
+        )
+    }
 };
